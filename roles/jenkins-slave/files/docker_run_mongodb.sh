07dac83f4f5a37727ffb2f95b0591285396a8759
Gunnar Morling <gunnar.morling@googlemail.com>
2015-03-23 15:13:41 +0100

    Adding --smallfiles option for MongoDB to save some space

--- roles/jenkins-slave/files/docker_run_mongodb.sh
+++ roles/jenkins-slave/files/docker_run_mongodb.sh
@@ -8,5 +8,4 @@ die () {
 [ "$#" -eq 1 ] || die "1 argument required, $# provided"
 echo $1 | grep -E -q '^[0-9]+$' || die "Numeric argument required, provided: $1"
 
-docker run -d -t -p $1:27017 dockerfile/mongodb
-
+docker run -d -t -p $1:27017 dockerfile/mongodb mongod --smallfiles
